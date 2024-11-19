104296b43b3b36c4c64a5417de52a3d37dd4acb1
yeainlee <137269761+yeainlee@users.noreply.github.com>
2024-11-19 20:56:27 +0900

    Update DFS와 BFS.cc

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -3,67 +3,87 @@
 #include <algorithm>
 #include <string>
 #include <queue>
-#include <vector>
-#include <cmath>
 
 using namespace std;
+queue <int> q;
+queue <int> q2;
+queue <int> bb;
 
-vector<int> vec[10002];
-vector<int> result_bfs;
-vector<int> result_dfs;
-bool visit[1002];
+#define MAX 1001
 
-void bfs(int temp) {
-	queue<int> q;
-	q.push(temp);
-	visit[temp] = true;
-	while (!q.empty()) {
-		int x = q.front();
-		q.pop();
-		result_bfs.push_back(x);
+int n, m, v;
+int arr[MAX][MAX];
+bool check[MAX];
+bool check2[MAX];
 
-		for (int i = 0; i < vec[x].size(); i++) {
-			if (!visit[vec[x][i]]) {
-				q.push(vec[x][i]);
-				visit[vec[x][i]] = true;
+void BFS(int kk) {
+	check2[kk] = true;
+	bb.push(kk);
+	while (!bb.empty()) {
+		kk = bb.front();
+		bb.pop();
+		for (int i = 1; i <= n; i++) {
+			if (arr[kk][i] == 1) {
+				if (check2[i] == false) {
+					check2[i] = true;
+					bb.push(i);
+					q2.push(i);
+				}
+			}
+		}
+	}
+}
+
+void DFS(int kk) {
+	for (int i = 1; i <= n; i++) {
+		if (arr[kk][i] == 1){
+			if (check[i] == false){
+				q.push(i);
+				check[i] = true;
+				DFS(i);
+			}
+		}
+	}
+}
+
+
+int main() {
+	cin >> n >> m >> v;
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+
+	//DFS
+	q.push(v);
+	check[v] = true;
+	for (int i = 1; i <= n; i++) {
+		if (arr[v][i] == 1) {
+			if (check[i] == false) {
+				check[i] = true;
+				q.push(i);
+				DFS(i);
 			}
 		}
 	}
 
-}
-
-void dfs(int x) {
-	visit[x] = true;
-	result_dfs.push_back(x);
-
-	for (int i = 0; i < vec[x].size(); i++) {
-		if (!visit[vec[x][i]]) {
-			dfs(vec[x][i]);
-		}
-	}
-}
-
-int main() {
-	int n, m, v, a, b; //n : 정점의 개수 m: 간선의 개수 v:탐색을 시작할 정점의 번호
-	cin >> n >> m >> v;
-
-	for (int i = 1; i <= m; i++) {
-		cin >> a >> b;
-		vec[a].push_back(b); //양방향 간선처리
-		vec[b].push_back(a);
-	}
-	for (int i = 1; i <= n; i++) {
-		sort(vec[i].begin(), vec[i].end()); //낮은 숫자부터 탐색
-	}
-	bfs(v);
-	memset(visit, false, sizeof(visit));
-	dfs(v);
-	for (int i = 0; i < result_dfs.size(); i++) {
-		cout << result_dfs[i] << " ";
+	while(!q.empty()) {
+		int nn = q.front();
+		q.pop();
+		cout << nn << " ";
 	}
 	cout << endl;
-	for (int i = 0; i < result_bfs.size(); i++) {
-		cout << result_bfs[i] << " ";
+
+	//BFS
+	q2.push(v);
+	BFS(v); 
+
+	while (!q2.empty()) {
+		int nn = q2.front();
+		q2.pop();
+		cout << nn << " ";
 	}
 	return 0;
 }
