75c65f3db66c2a3207b8bee382dd4f8bcbdeab15
yeainlee <137269761+yeainlee@users.noreply.github.com>
2024-11-25 16:26:15 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+	long long arr[91];
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	cout << arr[n] << endl;
+	return 0;
+}
